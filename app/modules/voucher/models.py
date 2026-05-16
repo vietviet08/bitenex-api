48a3182b82499f31db51a744f74dd24fba486f8d
vietviet08 <vietnq.23ceb@vku.udn.vn>
2026-05-16 21:47:45 +0700

    Revert "feat: improve order status"
    This reverts commit 7317ae8bab2edb28031508f208096d7519613bf0.

--- app/modules/voucher/models.py
+++
@@ -1,7 +0,0 @@
-# =============================================================================
-# Voucher Module - ORM Models
-# =============================================================================
-
-from app.modules.journey.models import JourneyOffer
-
-__all__ = ["JourneyOffer"]
